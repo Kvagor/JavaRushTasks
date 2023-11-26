9bbb2e7c9d21db896b4cc1f63b0893cd81f301e5
Egor Belonozhenko <egor.belonozhenko.04@mail.ru>
2023-11-26 20:49:14 +0300

    add solution JavaSyntax task0410

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.pro.task06.task0605;
+
+/* 
+Правильный порядок
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        //напишите тут ваш код
+        //int [] array2=Arrays.copyOf(array,array.length);
+        //for
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
+        }
+        
+        /*int rev=0;
+        for (int i = array.length-1; i >=0 ; i--) {
+            array[rev]=array[i];
+            rev++;
+            
+        }*/
+
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
