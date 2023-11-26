c61d25e32f521b039fab17cdee6de94d64df921d
Egor Belonozhenko <egor.belonozhenko.04@mail.ru>
2023-11-26 22:12:58 +0300

    add solution JavaSyntax task1006

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1006;
+
+/* 
+Кто тут наследник?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        HybridCar hb = new HybridCar();
+        GasCar gs = new GasCar();
+        ElectricCar ec = new ElectricCar();
+    }
+}
